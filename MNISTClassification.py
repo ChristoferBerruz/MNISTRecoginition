374d9df4067e313322f116b21d156766062a44c7
ChristoferBerruz <46355467+ChristoferBerruz@users.noreply.github.com>
2020-03-19 21:25:00 -0400

    Driver for Digit Classification

--- MNISTClassification.py
+++ MNISTClassification.py
@@ -2,6 +2,7 @@ import os
 import sys
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from NN import NN
 
@@ -26,10 +27,40 @@ def loadMatrices(): #Loading images
         test[i] = cv2.imread('C:/Users/chris/Downloads/Data/Data/Test10000/{}'.format(fname), 0)/255.0
         i += 1
     trainX = train.reshape(train.shape[0], train.shape[1]*train.shape[2], 1)
-    return trainX, trainY, np.reshape(test,(test.shape[0], test.shape[1]*test.shape[2], 1)), testY
+    testX = test.reshape(test.shape[0],test.shape[1]*test.shape[2],1)
+    return trainX, trainY, testX, testY
+
 def main():
     trainX, trainY, testX, testY = loadMatrices()
-    nn = NN(100, 10, 784)
-    nn.train(trainX, trainY, 0.01, 5)
-    nn.test_model(testX, testY)
+    DigitNN = NN(100, 10, 784)
+    DigitNN.train_by_SGD(trainX, trainY, 0.01, 100)
+    DigitNN.test_model(testX, testY)
+
+def plot_comparison(trainX, trainY, testX, testY):
+    epochs = [25, 50, 100, 150]
+    l1_vals = [25, 50, 100, 150]
+    fig, a = plt.subplots(2, 2)
+    for j in range(len(l1_vals)):
+        sgd = []
+        mini_batch = []
+        for i in range(len(epochs)):
+            l1 = l1_vals[j]
+            epoch = epochs[i]
+            nn = NN(l1, 10, 784)
+            nn.train_by_SGD(trainX, trainY, 0.01, epoch)
+            acc1 = nn.test_model(testX, testY)
+            sgd.append(acc1)
+            nn = NN(l1, 10, 784)
+            nn.train_by_MBGD(trainX, trainY, 0.01, epoch)
+            acc2 = nn.test_model(testX, testY)
+            mini_batch.append(acc2)
+        idx = 0
+        if j >= 2:
+            idx = 1
+        a[idx][j%2].scatter(epochs, sgd, label = 'SGD')
+        a[idx][j%2].scatter(epochs, mini_batch, label = 'Mini Batch')
+        a[idx][j%2].set_title('Neurons in Hidden Layer = %d'%l1_vals[j])
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':main()
